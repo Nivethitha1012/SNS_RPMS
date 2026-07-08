8ccc349c2ba745dc28ddc85b6e8c4b393a68a553
Arunpandiyan SNS <arunpandiyan.s.ihub@snsgroups.com>
2026-07-08 16:24:01 +0530

    Delete old backend directory

--- new backend/route/auth_route.js
+++
@@ -1,9 +0,0 @@
-// routes/authRoutes.js
-
-const express = require("express");
-const router = express.Router();
-const authController = require("../controller/authcontroller.js");
-
-router.post("/sso-login", authController.ssoLogin);
-
-module.exports = router;
